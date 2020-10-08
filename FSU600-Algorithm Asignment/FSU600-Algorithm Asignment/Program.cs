4310fab12392c5b741bb2b112c9d6803b25b8462
Samira Abdoljavadi <saab0029@student.hv.se>
2020-10-08 17:11:09 +0200

    Create Console Solution

---
+++ FSU600-Algorithm Asignment/FSU600-Algorithm Asignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSU600_Algorithm_Asignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
