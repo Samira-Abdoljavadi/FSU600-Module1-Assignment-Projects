c13c1d253f49e3d0eddcfd8adcc7b96f07f8dab2
Samira Abdoljavadi <saab0029@student.hv.se>
2020-10-08 17:17:38 +0200

    Write Methods Swap, Randomize and Prepare

--- FSU600-Algorithm Asignment/Algorithm/Class1.cs
+++ FSU600-Algorithm Asignment/Algorithm/Class1.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Algorithm
 {
-    public class Class1
+    //Create a dynamic link library with the name Algorithms that contains
+    public class Algorithmclass1
     {
+
+        //A method Swap to swap two data values at two positions in a given integer array.
+        public static void Swap(int[] MyArray, int m, int n)
+        {
+            int temp;
+            temp = MyArray[m];
+            MyArray[m] = MyArray[n];
+            MyArray[n] = temp;
+        }
+
+        //A method Randomize to create the random array of data, random numbers are between 0 and 10 times the array size.
+        public static void Randomize(int[] MyArray)
+        {
+            Random randNum = new Random();
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                MyArray[i] = randNum.Next(0, MyArray.Length * 10);
+            }
+        }
+
+        //A method Prepare that takes an array size n as a parameter. And create an array of integers with this specific size. and then call the Randomize passing it the array.
+        public static void Prepare(int ArraySize)
+        {
+            int[] MyArray = new int[ArraySize];
+            Randomize(MyArray);
+            Console.WriteLine(string.Format("Here's the Random Array: [{0}].", string.Join(", ", MyArray)));
+        }
     }
 }
--- FSU600-Algorithm Asignment/FSU600-Algorithm Asignment/Program.cs
+++ FSU600-Algorithm Asignment/FSU600-Algorithm Asignment/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithm;
 
 namespace FSU600_Algorithm_Asignment
 {
@@ -10,6 +11,13 @@ namespace FSU600_Algorithm_Asignment
     {
         static void Main(string[] args)
         {
+            int ArraySize;
+            string ch;
+            Console.WriteLine("Input The Array Size:");
+            ArraySize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Array Size is:" + ArraySize);
+            Algorithmclass1.Prepare(ArraySize);
+            ch = Console.ReadLine();
         }
     }
 }
